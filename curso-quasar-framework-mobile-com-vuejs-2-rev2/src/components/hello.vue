da19d51eb0bb41509ecdc2337c45d436bed17eb5
ValterFC <valter@accellog.com>
2018-09-13 18:08:32 -0300

    Initial Commit

---
+++ curso-quasar-framework-mobile-com-vuejs-2-rev2/src/components/hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Olá {{ name }},</h1>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {
+      name: 'Erik'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ curso-quasar-framework-mobile-com-vuejs-2-rev2/src/pages/welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <hello/>
+  </q-page>
+</template>
+
+<script>
+import Hello from '../components/hello';
+
+export default {
+  // name: 'PageName',
+  components: {
+    'hello': Hello
+  }
+}
+</script>
+
+<style>
+</style>
